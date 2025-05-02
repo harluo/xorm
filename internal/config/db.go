a518de261afa7de37a6217d830b1553079abac0d
storezhang <storezhang@gmail.com>
2025-05-02 16:14:14 +0800

    feat(依赖): 升级到最新版本

--- internal/config/db.go
+++ internal/config/db.go
@@ -62,13 +62,16 @@ type DB struct {
 	Sqlite internal.Sqlite `json:"sqlite,omitempty"`
 }
 
-func newDB(getter config.Getter) (db *DB, err error) {
+func newDB(getter config.Getter) (db *DB, typ db.Type, err error) {
 	db = new(DB)
 	err = getter.Get(&struct {
 		DB *DB `json:"db,omitempty" validate:"required"`
 	}{
 		DB: db,
 	})
+	if nil == err { // 暴露数据库类型，供其它项目使用
+		typ = db.Type
+	}
 
 	return
 }
