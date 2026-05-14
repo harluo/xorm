f6dc06ee1db6fb5377dc999daa3201a1d992136f
storezhang <storezhang@gmail.com>
2026-05-14 10:37:57 +0800

    feat(依赖): 升级到最新版

---
+++ model.go
@@ -0,0 +1,14 @@
+package xorm
+
+import (
+	"time"
+)
+
+type Model struct {
+	Identity `xorm:"extends"`
+
+	// 创建时间
+	Created time.Time `xorm:"created notnull default(CURRENT_TIMESTAMP) comment('创建时间，创建时自动设置')" json:"created,omitempty"` // nolint:lll
+	// 最后更新时间
+	Updated time.Time `xorm:"updated notnull default(CURRENT_TIMESTAMP) comment('最后更新时间，每次更新时自动修改')" json:"updated,omitempty"` // nolint:lll
+}
