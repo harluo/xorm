b905b3557a88ea7438fd3483d2dc6a85281d9059
storezhang <storezhang@gmail.com>
2026-07-28 10:27:27 +0800

    📝 docs(deleted_at): 添加结构体注释并将 Deleted 字段改为指针类型

--- deleted_at.go
+++ deleted_at.go
@@ -4,8 +4,9 @@ import (
 	"time"
 )
 
+// DeletedAt 用于实现软删除
 type DeletedAt struct {
 	Model `xorm:"extends"`
 
-	Deleted time.Time `xorm:"deleted comment('最后删除时间，用于实现软删除功能')" json:"deleted,omitempty"`
+	Deleted *time.Time `xorm:"deleted comment('最后删除时间，用于实现软删除功能')" json:"deleted,omitempty"`
 }
